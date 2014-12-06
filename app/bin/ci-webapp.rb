2873844405bb29a778daf9df543341c3ab515f1b
Graeme Lockley <graemel@no9.co.za>
2014-12-06 16:00:35 +0200

    Moving workspace around rather than directory names.  The intent is to capture all of the directory methods into workspace rather than splattering them all around the app.

--- app/bin/ci-webapp.rb
+++ app/bin/ci-webapp.rb
@@ -10,59 +10,70 @@ set :public_folder, File.dirname(__FILE__) + '/../data'
 set :bind, '0.0.0.0'
 
 class Workspace
-	def initialize(home_dir)
-		@home_dir = home_dir
-	end
+  def initialize(home_dir)
+    @home_dir = home_dir
+  end
 
-	def projects
-		Projects.new @home_dir
-	end
+  def project_names
+    Dir[@home_dir + "/*"].map{ |dir| dir.split("/").last }
+  end
+
+  def projects
+    Projects.new self
+  end
 end
 
 class Projects
-	def initialize(home_dir)
-		@home_dir = home_dir
-	end
+  def initialize(workspace)
+    @workspace = workspace
+  end
 
-	def all
-		Dir[@home_dir + "/*"].map { |t| Project.new t.split("/").last }
-	end
+  def all
+    @workspace.project_names.map { |name| Project.new name }
+  end
 
-	def project(name)
-
-	end
+  def project(name)
+    Project.new name
+  end
 end
 
 class Project
-	def initialize(name)
-		@name = name
-	end
+  def initialize(name)
+    @name = name
+  end
 
+  def to_map
+    {:id => @name, :name => @name}
+  end
 
-	def to_map
-		{:id => @name, :name => @name}
-	end
+  def to_s
+    @name
+  end
 
-	def to_s
-		@name
-	end
+  def pipelines
+    Dir
+  end
+end
+
+class Pipeline
+
 end
 
 workspace = Workspace.new File.join (File.dirname(__FILE__) + "/../../workspace")
 
 get '/' do
-	File.read(File.join(File.dirname(__FILE__), '/../data', 'index.html'))
+  File.read(File.join(File.dirname(__FILE__), '/../data', 'index.html'))
 end
 
 get '/api/projects' do
-	content_type :json
+  content_type :json
 
-	workspace.projects.all.map{|x| x.to_map}.to_json
+  workspace.projects.all.map { |x| x.to_map }.to_json
 end
 
 get '/api/projects/:name' do
-	content_type :json
+  content_type :json
 
-	workspace.project(params[:name]).to_map.to_json
+  workspace.project(params[:name]).to_map.to_json
 end
 

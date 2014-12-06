f66ef24ea3741eab1f02bcbdf12ba14a89aa99cc
Graeme Lockley <graemel@no9.co.za>
2014-12-06 15:36:35 +0200

    Created a workspace class and moved the sinatra instructions from the top to the tail of the file.

--- app/bin/ci-webapp.rb
+++ app/bin/ci-webapp.rb
@@ -9,16 +9,14 @@ require 'json'
 set :public_folder, File.dirname(__FILE__) + '/../data'
 set :bind, '0.0.0.0'
 
-get '/' do
-	  File.read(File.join(File.dirname(__FILE__), '/../data', 'index.html'))
-end
+class Workspace
+	def initialize(home_dir)
+		@home_dir = home_dir
+	end
 
-get '/api/projects' do
-	content_type :json
-
-	projects = Projects.new File.join(File.dirname(__FILE__), "/../../workspace")
-
-	projects.all.map{|x| x.to_map}.to_json
+	def projects
+		Projects.new @home_dir
+	end
 end
 
 class Projects
@@ -29,12 +27,17 @@ class Projects
 	def all
 		Dir[@home_dir + "/*"].map { |t| Project.new t.split("/").last }
 	end
+
+	def project(name)
+
+	end
 end
 
 class Project
 	def initialize(name)
 		@name = name
 	end
+
 
 	def to_map
 		{:id => @name, :name => @name}
@@ -44,3 +47,22 @@ class Project
 		@name
 	end
 end
+
+workspace = Workspace.new File.join (File.dirname(__FILE__) + "/../../workspace")
+
+get '/' do
+	File.read(File.join(File.dirname(__FILE__), '/../data', 'index.html'))
+end
+
+get '/api/projects' do
+	content_type :json
+
+	workspace.projects.all.map{|x| x.to_map}.to_json
+end
+
+get '/api/projects/:name' do
+	content_type :json
+
+	workspace.project(params[:name]).to_map.to_json
+end
+

e5c1899db1f78d3eefda9a7ea12298a3bdc82bd3
Graeme Lockley <graemel@no9.co.za>
2014-12-14 10:23:06 +0200

    Added method to return the class name of the task and used in displaying the pipeline.

--- app/data/js/controllers.js
+++ app/data/js/controllers.js
@@ -11,8 +11,27 @@ ciControllers.controller("ProjectsCtrl", function ($scope, $http) {
 ciControllers.controller("ProjectDetailCtrl", function ($scope, $http, $routeParams) {
     $scope.projectID = $routeParams.projectID;
 
-    var promise = $http.get("/api/projects/" + $scope.projectID + "/pipelines")
+    var promise = $http.get("/api/projects/" + $scope.projectID + "/pipelines");
     promise.success(function (response) {
         $scope.project = response;
     });
+
+    $scope.taskMarkup = function (pipelineStatus, pipelineTask, step) {
+        if (pipelineStatus == 'ready') {
+            return 'bg-active';
+        } else if (pipelineStatus == 'complete') {
+            return 'bg-success';
+        } else if (pipelineTask == step.name) {
+            if (pipelineStatus == 'running') {
+                return 'bg-warning';
+            } else {
+                return 'bg-danger';
+            }
+        } else if (step.name < pipelineTask) {
+            return 'bg-success';
+        } else {
+            console.log(pipelineStatus, pipelineTask, step);
+            return 'bg-active';
+        }
+    };
 });
